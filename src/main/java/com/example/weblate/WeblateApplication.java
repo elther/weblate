6003301dde8d933ecc43c517a5f6e920c81e6ce7
ekcho <ekcho@ncsoft.com>
2022-05-11 20:09:01 +0900

    Initial commit

---
+++ src/main/java/com/example/weblate/WeblateApplication.java
@@ -0,0 +1,13 @@
+package com.example.weblate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeblateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeblateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/weblate/WeblateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.weblate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeblateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
